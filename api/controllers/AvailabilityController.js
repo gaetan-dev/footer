b1acf143efbb0205228bc5d8d77e92e53f74b9f4
vikingdufutur <kraw@live.fr>
2015-10-29 11:50:03 +0100

    Fix problem: Availabilities multi-update

--- api/controllers/AvailabilityController.js
+++ api/controllers/AvailabilityController.js
@@ -6,81 +6,94 @@
  */
 
 var find = function (req, res) {
-	if (req.param('user_id')) { return findUserId(req, res); }  
-	
-	AvailabilityService.find(function (err, availabilities) {
-		if (err) res.json(err, 500);
-		else if (availabilities) {
-			res.json(availabilities);
-		} else {
-			res.json({ message: 'Availabilities not found' }, 404);
-		}
-	});
+  if (req.param('user_id')) {
+    return findUserId(req, res);
+  }
+
+  AvailabilityService.find(function (err, availabilities) {
+    if (err) res.json(err, 500);
+    else if (availabilities) {
+      res.json(availabilities);
+    } else {
+      res.json({
+        message: 'Availabilities not found'
+      }, 404);
+    }
+  });
 };
 
 var findOne = function (req, res) {
-	AvailabilityService.findOne(req.param('id'), function (err, availability) {
-		if (err) res.json(err, 500);
-		else if (availability) {
-			res.json(availability)
-		} else {
-			res.json({ message: 'Availability not found' }, 404);
-		}
-	});
+  AvailabilityService.findOne(req.param('id'), function (err, availability) {
+    if (err) res.json(err, 500);
+    else if (availability) {
+      res.json(availability);
+    } else {
+      res.json({
+        message: 'Availability not found'
+      }, 404);
+    }
+  });
 };
 
-var findUserId = function (req, res) {	
-	AvailabilityService.findUserId(req.param('user_id'), function(err, availabilities) {
-		if (err) res.json(err, 500);
-		else if (availabilities) {
-			res.json(availabilities)
-		} else {
-			res.json({ message: 'Availabilities not found' }, 404);
-		}
-	});
+var findUserId = function (req, res) {
+  AvailabilityService.findUserId(req.param('user_id'), function (err, availabilities) {
+    if (err) res.json(err, 500);
+    else if (availabilities) {
+      res.json(availabilities);
+    } else {
+      res.json({
+        message: 'Availabilities not found'
+      }, 404);
+    }
+  });
 };
 
 var createOne = function (req, res) {
-	AvailabilityService.create(req.body, function (err, availability) {
-		if (err) res.json(err, 500);
-		else if (availability) {
-			res.json(availability)
-		} else {
-			res.json({ message: 'Availability not create' }, 404);
-		}
-	});
+  AvailabilityService.create(req.body, function (err, availability) {
+    if (err) res.json(err, 500);
+    else if (availability) {
+      res.json(availability);
+    } else {
+      res.json({
+        message: 'Availability not create'
+      }, 404);
+    }
+  });
 };
 
 var create = function (req, res) {
-	AvailabilityService.create(req.body, function (err, availability) {
-		if (err) res.json(err, 500);
-		else if (availability) {
-			res.json(availability)
-		} else {
-			res.json({ message: 'Availability not create' }, 404);
-		}
-	});
+  AvailabilityService.create(req.body, function (err, availability) {
+    if (err) res.json(err, 500);
+    else if (availability) {
+      res.json(availability);
+    } else {
+      res.json({
+        message: 'Availability not create'
+      }, 404);
+    }
+  });
 };
 
 var update = function (req, res) {
-	AvailabilityService.update(req.body, function (err, availabilities) {
-		if (err) res.json(err, 500);
-		else if (availabilities) {
-			res.json(availabilities)
-		} else {
-			res.json({ message: 'availabilities not update' }, 404);
-		}
-	});
+  AvailabilityService.update(req.body, function (err, availabilities) {
+    if (err) res.json(err, 500);
+    else if (availabilities) {
+      res.json(availabilities);
+    } else {
+      res.json({
+        message: 'availabilities not update'
+      }, 404);
+    }
+  });
 };
 
 module.exports = {
-		
-	/* Methodes */
-	find: find,
-	findUserId: findUserId,
-	findOne: findOne,
-	create: create,
-	update: update
-	
-};
 
+  /* Methodes */
+  find: find,
+  findUserId: findUserId,
+  findOne: findOne,
+  create: create,
+  update: update
+
+};
--- api/services/AvailabilityService.js
+++ api/services/AvailabilityService.js
@@ -1,131 +1,126 @@
 /**
-* AvailabilityService.js
-*/
+ * AvailabilityService.js
+ */
 
 var find = function (callback) {
-	sails.log.debug('******************* Availability.find()');
-	Availability.find().exec(function (err, availabilities) {
-		callback(err, availabilities);
-	});
+  sails.log.debug('******************* Availability.find()');
+  Availability.find().exec(function (err, availabilities) {
+    callback(err, availabilities);
+  });
 };
 
 var findUserId = function (user_id, callback) {
-	sails.log.debug('******************* Availability.findUserId(' + user_id + ')');
-	var query = {
-		bool: {
-			must: [
-				{
-					term: {
-						'availability.user.id': user_id
-					}
-				}
-			]
-		}
-	};
+  sails.log.debug('******************* Availability.findUserId(' + user_id + ')');
+  var query = {
+    bool: {
+      must: [{
+        term: {
+          'availability.user': user_id
+        }
+      }]
+    }
+  };
 
-	Availability.find()
-		.where(query)
-		.limit(100000)
-		.exec(function (err, availabilities) {
-			callback(err, availabilities);
-		});
+  Availability.find()
+    .where(query)
+    .limit(100000)
+    .exec(function (err, availabilities) {
+      callback(err, availabilities);
+    });
 };
-	
-var findOne = function (id, callback) {
-	sails.log.debug('******************* Availability.findOne(' + id + ')');
-	var query = {
-		bool: {
-			must: [
-				{
-					term: {
-						'availability.id': id
-					}
-				}
-			]
-		}
-	};
 
-	Availability.findOne(query).exec(function (err, availability) {
-		callback(err, availability);
-	});
+var findOne = function (id, callback) {
+  sails.log.debug('******************* Availability.findOne(' + id + ')');
+  var query = {
+    bool: {
+      must: [{
+        term: {
+          'availability.id': id
+        }
+      }]
+    }
+  };
+
+  Availability.findOne(query).exec(function (err, availability) {
+    callback(err, availability);
+  });
 };
 
 var createOne = function (body, callback) {
-	sails.log.debug('******************* Availability.createOne()');
+  sails.log.debug('******************* Availability.createOne()');
 
-	UserService.findOne(body.user, function (err, user) {
-		
-		body.user = user;
-		
-		Availability.create(body).exec(function (err, availability) {
-			callback(err, availability);
-		});
-	});
+  UserService.findOne(body.user, function (err, user) {
+
+    body.user = user;
+
+    Availability.create(body).exec(function (err, availability) {
+      callback(err, availability);
+    });
+  });
 };
 
 var create = function (body, callback) {
-	sails.log.debug('******************* Availability.create()');
-	
-	var availabilities = body.availabilities;
-	if (availabilities.length > 0) {
-		UserService.findOne(availabilities[0].user, function (err, user) {
-			
-			// Add the current user
-			for (var i = 0; i < availabilities.length; i++) {
-				availabilities[i].user = user;
-			}			
-			
-			Availability.create(availabilities).exec(function (err, availability) {
-				callback(err, availability);
-			});
-		});
-	}
+  sails.log.debug('******************* Availability.create()');
+
+  var availabilities = body.availabilities;
+  Availability.createEach(availabilities).exec(function (err, availabilities) {
+    callback(err, availabilities);
+  });
 };
 
 var update = function (body, callback) {
-	sails.log.debug('******************* Availability.update()');
-	
-	updateLoop(body.availabilities, callback);
+  sails.log.debug('******************* Availability.update()');
+
+  ids = [];
+  for (var i = 0; i < body.availabilities.length; i++) {
+    ids.push(body.availabilities[i].id);
+  }
+
+  var query = {
+    bool: {
+      should: [{
+        terms: {
+          'availability.id': ids
+        }
+      }]
+    }
+  };
+
+  Availability.update(query, {
+    mode: body.mode
+  }).exec(function (err, availabilities) {
+    callback(err, availabilities);
+  });
 };
 
-var updateOne = function (availability, loop, callback) {
-	sails.log.debug('******************* Availability.updateOne(' + availability.id + ')');
-	var query = {
-		bool: {
-			must: [
-				{
-					term: {
-						'availability.id': availability.id
-					}
-				}
-			]
-		}
-	};
+var updateOne = function (availability, res, err) {
+  sails.log.debug('******************* Availability.updateOne(' + availability.id + ')');
 
-	Availability.update(query, {mode: availability.mode}).exec(function (err, availability) {
-		if (err) { callback(err, availability); }
-		else if (availability) { 
-			loop();
-		}	
-	});
-};
+  var query = {
+    bool: {
+      must: [{
+        term: {
+          'availability.id': availability.id
+        }
+      }]
+    }
+  };
 
-var updateLoop = function (availabilities, callback) {
-	if (availabilities.length > 0) {
-		var availability = availabilities.splice(0, 1)[0];
-		
-		var loop = function () {
-			updateLoop(availabilities, callback);
-		};
-		
-		updateOne(availability, loop, callback);	
-	}
+  Availability.update(query, {
+    mode: availability.mode
+  }).exec(function (err, res) {
+    if (err) {
+      err = err;
+    } else if (availability) {
+      res = availability;
+    }
+  });
 };
 
 module.exports = {
-	find: find,
-	findUserId: findUserId,
-	findOne: findOne,
-	create: create,
-	update: update,
-}
+  find: find,
+  findUserId: findUserId,
+  findOne: findOne,
+  create: create,
+  update: update,
+};
